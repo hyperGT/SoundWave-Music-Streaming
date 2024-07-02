8a260f7be3e9e26c6465190886c526b3e436f9b4
hyperGT <charlesgabriel102@gmail.com>
2024-07-02 18:58:42 -0300

    v1.0 finalizado

--- README.md
+++
@@ -1,1 +0,0 @@
-# SoundWave-Music-Streaming
--- js/script.js
+++ js/script.js
@@ -1,49 +1,41 @@
-$(function() {    
+$(function(){
 
+    var hidden = false
+    $("#esconder-aside").click(function () {
+        $("aside").toggle();
 
-var hidden = false
-    $("#esconder-aside").click(function() {
-        $("aside").toggle();  
-        
-        if(hidden==false){
+        if (hidden == false) {
 
             $(".separar").css("width", "100%");
-            /*$("main").css("width", "100%");*/
-/*
-            $(".container").css({
-                "width": "80%",
-                "margin-left": "10em"                
-            });   
-  */          
-            hidden = true;
-        }else{
-            $(".separar").css("width", "80%");
-            /*$("main").css("width", "80%");*/
 
-            $(".container").css({
-                "width": "100%", 
-                "margin-left": "1em",
-            }); 
+            hidden = true;
+
+        } else {
+            $(".separar").css("width", "80%");
 
             hidden = false;
+
         }
-    
+
     });
 
-var darkTheme = true;
-    $("#mudar-tema").click(function() {
-        if(darkTheme==false){ // verificando se o escuro está ativado
+    var darkTheme = true;
+    $("#mudar-tema").click(function () {
+        if (darkTheme == false) { // verificando se o escuro está ativado
 
-            // trocando o atributo src das imagens dos botões da nav bar
-            $("#tema").attr("src", "imgs/buttons/sun.png");            
+            // trocando o src das imagens dos botões da nav bar 
+            $("#tema").attr("src", "imgs/icons/sun.png");
 
-            $("#esconder").attr("src", "imgs/buttons/burger dark.png");
+            $("#esconder").attr("src", "imgs/icons/burger dark.png");
 
-            $("#lupa").attr("src", "imgs/buttons/search dark.png");
+            $("#lupa").attr("src", "imgs/icons/search dark.png");
 
             // mudando as cores para o tema escuro
-            $("figcaption").css("color", "white");
-            
+
+            $("a").css("color", "white");
+
+            $(".perfil-name").css("color", "white");
+
             $("header").css({
                 "background-color": "#333"
             });
@@ -54,30 +46,33 @@ var darkTheme = true;
             });
 
             $("aside").css({
-                "background-color": "#333"
+                "background-color": "#222121"
             });
 
             $("footer").css({
                 "background-color": "#333"
             });
-                        
+
             darkTheme = true;
 
-        }else{ // darktheme está ativo
+        } else { // tema escuro está ativo
+            
+            // trocando o src das imagens dos botões da nav bar 
+            $("#tema").attr("src", "imgs/icons/moon.png");
 
-            // trocando o atributo src da imagem do botão trocar tema
-            $("#tema").attr("src", "imgs/buttons/moon.png");
+            $("#esconder").attr("src", "imgs/icons/burger light.png");
 
-            $("#esconder").attr("src", "imgs/buttons/burger light.png");
-
-            $("#lupa").attr("src", "imgs/buttons/search light.png");
+            $("#lupa").attr("src", "imgs/icons/search light.png");
 
             // mudando as cores para o tema claro
-            $("figcaption").css("color", "#1c1d1c");
+
+            $("a").css("color", "#1c1d1c");
 
             $("header").css({
                 "background-color": "#3CB371"
             });
+
+            $(".perfil-name").css("color", "black");
 
             $("body").css({
                 "background-color": "#fff",
@@ -97,7 +92,7 @@ var darkTheme = true;
     });
 });
 
-function barraSel(){
+function barraSel() {
     $("#barraDePesquisa").css({
         "border-width": "5px",
         "border-style": "solid",
@@ -105,40 +100,35 @@ function barraSel(){
     });
 }
 
-function barraNotSel(){
+function barraNotSel() {
     $("#barraDePesquisa").css("border", "hidden");
 }
 
-/**
- * Eventos
- * Ativar lista ordenada de musicas do album + audio controler quando clicar 
- * em alguma musica
- */
+// playlist 
+document.addEventListener('DOMContentLoaded', function () {
 
-document.addEventListener('DOMContentLoaded', function(){
-    
-    const audioPlayer = document.getElementById("audioPlayer"); 
+    const audioPlayer = document.getElementById("audioPlayer");
     const audioSource = document.getElementById("audioSource");
     const playlist = document.querySelector(".lista-de-musicas");
-    const musicLinks = document.querySelectorAll(".lista-de-musicas a"); 
+    const musicLinks = document.querySelectorAll(".lista-de-musicas a");
 
     // escondendo a lista de musicas e o controlador de audio
     playlist.style.display = "none";
-    audioPlayer.style.display="none";
+    audioPlayer.style.display = "none";
 
     // mostrando tudo ao clicar em um album (atualmente só o Thriller disponivel para teste)
-    albumLinkTeste.addEventListener("click", function(){
-        playlist.style.display = "block"; 
+    albumLinkTeste.addEventListener("click", function () {
+        playlist.style.display = "block";
         audioPlayer.style.display = "block";
-                
+        $(".sideBarMusica h1#nomeAlbum").text("Album: Thriller");
     });
 
     musicLinks.forEach(link => {
         // quando clicar na âncora inicia a função
-        link.addEventListener("click", function(event){
+        link.addEventListener("click", function (event) {
             event.preventDefault();
             // guardando na variavel musicSrc o caminho em que a musica está armazenada 
-            const musicSrc = this.getAttribute("data-src"); 
+            const musicSrc = this.getAttribute("data-src");
             // mudando o atributo src do componente audioSource para o caminho da musica
             audioSource.src = musicSrc;
             // tocando a musica chamando as funções "carregar" e "play" para tocar a música
@@ -147,3 +137,37 @@ document.addEventListener('DOMContentLoaded', function(){
         });
     });
 });
+
+// Carrossel
+let slideAtual = 0;
+
+function mostrarSlide(index) {
+    const slides = document.querySelectorAll(".video");
+    const carrossel = document.querySelector(".carrossel");
+
+    if (index >= slides.length) {
+        slideAtual=0;
+    } else if (index < 0) {
+        slideAtual = slides.length - 1;
+    } else {
+        slideAtual = index;
+    }
+
+    const offset = -slideAtual * 100; // Calcula o deslocamento para o slide atual
+    carrossel.style.transform = `translateX(${offset}%)`;
+}
+
+function proxSlide() {
+    mostrarSlide(slideAtual + 1);
+}
+
+function prevSlide() {
+    mostrarSlide(slideAtual - 1);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    mostrarSlide(slideAtual);
+});
+
+document.querySelector(".next").addEventListener("click", proxSlide);
+document.querySelector(".prev").addEventListener("click", prevSlide);
